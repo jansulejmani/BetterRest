11bf00dd7848fd7abd268e4dd1cbde1888a0cb29
Jan Sulejmani <64248502+jansulejmani@users.noreply.github.com>
2023-03-10 07:34:24 -0600

    Add files via upload

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jan Sulejmani on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
